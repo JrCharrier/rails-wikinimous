dcbcaf3ca10bd5c7cc8678a1d4b544b38e0cedfc
JeanRemi Charrier <jeanremi.char@gmail.com>
2024-11-12 16:17:32 +0100

    rails wikinimous done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "faker"
+
+10.times do
+  post = Article.new(
+    title: Faker::Book.title,
+    content: Faker::Quote.yoda
+  )
+  post.save!
+end
